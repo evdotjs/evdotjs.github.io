e6db25f6fbd83068d6bc8d1db77404b3d2d4f7ab
Ev McNeal <evmcneal@icloud.com>
2020-02-28 20:46:25 -0800

    change buttons to dropdown menus; update todo-list thumbnail

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 const projectList = [
   {
           title: 'Google Homepage',
+    description: 'a recreation of Google’s homepage using HTML and CSS',
            link: 'https://evdotjs.github.io/my-projects/google-homepage/',
      repository: 'https://github.com/evdotjs/my-projects/tree/gh-pages/google-homepage',
       thumbnail: 'img/google-homepage.png',
-            alt: 'google-homepage',
-    description: 'a recreation of Google’s homepage using HTML and CSS'
+            alt: 'google-homepage'
   },
   {
           title: 'Firefox Browser',
+    description: 'a recreation of the Mozilla Firefox download page',
            link: 'https://evdotjs.github.io/my-projects/firefox-browser/',
      repository: 'https://github.com/evdotjs/my-projects/tree/gh-pages/firefox-browser',
       thumbnail: 'img/firefox-browser.png',
-            alt: 'firefox-browser',
-    description: 'a recreation of the Mozilla Firefox download page'
+            alt: 'firefox-browser'
   },
   {
           title: 'Calculator',
+    description: 'a simple calculator using HTML, SASS, and Javascript',
            link: 'https://evdotjs.github.io/my-projects/calculator/',
      repository: 'https://github.com/evdotjs/my-projects/tree/gh-pages/calculator',
       thumbnail: 'img/calculator.png',
-            alt: 'calculator',
-    description: 'a simple calculator using HTML, SASS, and Javascript'
+            alt: 'calculator'
   },
   {
           title: 'To-Do List',
+    description: 'a to-do list app using HTML, SASS, and Vue.',
            link: 'https://evdotjs.github.io/my-projects/todo-list/',
      repository: 'https://github.com/evdotjs/my-projects/tree/gh-pages/todo-list',
-      thumbnail: 'img/todo-list.png',
-            alt: 'todo-list',
-    description: 'a to-do list app using HTML, SASS, and Javascript'
+      thumbnail: 'img/todo-list-v2.png',
+            alt: 'todo-list'
   }
 ];
 
